9b9c6dc6531abdce96a372b8e6f6b1f5298dc1f9
Max Losch <mmlosch@kth.se>
2017-02-28 15:18:22 +0100

    Distinguish render size from output image size. Render via matplotlib

--- gym_shapesort/envs/reacher.py
+++ gym_shapesort/envs/reacher.py
@@ -6,15 +6,22 @@ import numpy as np
 from random import random
 from gym import spaces
 import pkg_resources
+import matplotlib.pyplot as plt
+from skimage.transform import resize as imresize
 
 	
 def randf(start, end):
 	return random()*(end-start)+start
 
 class Reacher(gym.Env):
-	metadata = {'render.modes': ['human'], 'video.frames_per_second': 30}
+	metadata = {'render.modes': ['human'], 'video.frames_per_second': 1}
 
-	def __init__(self, frame_skip=2, obs_type='rgb', render_width=112, render_height=112, pybullet_mode='SHARED_MEMORY'):
+	def __init__(self, 
+		frame_skip=2, 
+		obs_type='rgb', 
+		render_width=256, render_height=256, 
+		image_width=112, image_height=112, 
+		pybullet_mode='SHARED_MEMORY'):
 
 		self.frame_skip = frame_skip
 		self.obs_type = obs_type
@@ -30,13 +37,18 @@ class Reacher(gym.Env):
 
 		self.render_width = render_width
 		self.render_height = render_height
+		self.image_width = image_width
+		self.image_height = image_height
 
-		cage_world.init()
+		self.plt_initialized = False
+		self.current_obs = None
+
+		# cage_world.init()
 
 		arm_desc = pkg_resources.resource_filename('gym_shapesort', 'description/gripper.urdf')
 		target_desc = pkg_resources.resource_filename('gym_shapesort', 'description/target.urdf')
 
-		self.body = gripper_interface.GripperInterface(p.loadURDF(arm_desc, [-0.55,-0.55,0]))
+		self.body = gripper_interface.GripperInterface(p.loadURDF(arm_desc, [-0.55,0,0]))
 		self.target = p.loadURDF(target_desc)
 
 		linkpos = self.body.getLinkStates()[0]
@@ -74,7 +86,7 @@ class Reacher(gym.Env):
 		if self.obs_type == 'rgbd':
 			channels = 4
 
-		obs = np.empty((self.render_height, self.render_width, channels*len(self.camera_pos)))
+		obs = np.empty((self.image_height, self.image_width, channels*len(self.camera_pos)))
 
 		target_pos = (0, 0, 0)
 		camera_up = (0, 0, 1)
@@ -93,9 +105,9 @@ class Reacher(gym.Env):
 
 			if self.obs_type == 'rgbd':
 				depth = depth.clip(0, 5)
-				rgba[:,:,3] = depth
+				rgba[:,:,3] = imresize(depth, (self.image_height, self.image_width))
 			else:
-				rgba = rgba[:,:,:3]  # slice off alpha, always 1.0
+				rgba = imresize(rgba[:,:,:3], (self.image_height, self.image_width))  # slice off alpha, always 1.0
 			obs[:,:,(i*channels):((i+1)*channels)] = rgba
 
 		# import matplotlib.pyplot as plt
@@ -122,13 +134,14 @@ class Reacher(gym.Env):
 		self._simulate(action)
 
 		ob = self._get_obs()
+		self.current_obs = ob
 
 		done = False
 		return ob, reward, done, dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl)
 
 	def _reset_target(self):
 		# randpos = np.array([randf(-0.5, 0.5), randf(-0.4, 0.4), randf(-0.5, 0.5)])
-		randpos = np.array([randf(-0.9, 0.9), randf(-0.9, 0.9), randf(-0.5, 0.5)])
+		randpos = np.array([randf(-0.5, 0.9), randf(-0.7, 0.7), randf(-0.5, 0.5)])
 		arm_root = np.array(p.getBasePositionAndOrientation(self.body.bodyid)[0])
 		
 		# ensure the target is reachable
@@ -153,7 +166,9 @@ class Reacher(gym.Env):
 		pos, _ = self.body.getJointStates()
 		self.body.reset(pos)
 
-		return self._get_obs()
+		ob = self._get_obs()
+		self.current_obs = ob
+		return ob
 		
 
 	def _open_visualizer(self):
@@ -162,18 +177,25 @@ class Reacher(gym.Env):
 
 	def _render(self, mode='human', close=False):
 		# rendering is done by bulletphysics if self.mode == p.GUI
-		# if not hasattr(self, 'display'):
-		# 	self._open_visualizer()
-
-		# self.window.clear()
-		# pyglet.sprite.Sprite(Image.fromarray(self._get_obs(), 'RGB')).draw()
-
-		# from scipy.misc import imshow
-		# imshow(self._get_obs())
+		if self.current_obs is None:
+			self._get_obs()
+		obs = self.current_obs
 		if self.obs_type == 'q':
-			obs = self._get_obs()
 			print('{:35s} | {:35s} | {:14s}\n{: 4.2f} {: 4.2f} {: 4.2f} {: 4.2f} {: 4.2f} {: 4.2f} | {: 4.2f} {: 4.2f} {: 4.2f} {: 4.2f} {: 4.2f} {: 4.2f} | {: 4.2f} {: 4.2f} {: 4.2f}'.format(
 				'Position', 'Velocity', 'Distance', obs[0], obs[1], obs[2], obs[3], obs[4], obs[5], obs[6], obs[7], obs[8], obs[9], obs[10], obs[11], obs[12], obs[13], obs[14]))
+		else:
+			if not self.plt_initialized:
+				plt.ion()
+				self.plt_im = plt.imshow(np.zeros((self.image_height, self.image_width, 3)))
+				self.plt_initialized = True
+			
+			self.plt_im.set_data(obs[:,:,:3])
+			plt.draw()
+			plt.pause(0.001)
+
+			if self.plt_initialized and close:
+				plt.ioff()
+				self.plt_initialized = False
 
 	@property
 	def action_space(self):
@@ -193,8 +215,8 @@ class Reacher(gym.Env):
 				channels = 4
 			elif self.obs_type == 'birgb':
 				channels = 6
-			high = np.ones((self.render_height, self.render_width, channels))
-			low  = -np.ones((self.render_height, self.render_width, channels))
+			high = np.ones((self.image_height, self.image_width, channels))
+			low  = -np.ones((self.image_height, self.image_width, channels))
 			return spaces.Box(low, high)
 
 	
